10b44d7bff8aaff6f4ead86db806d17001488e65
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-03-18 14:19:28 +0200

    add angulat material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialrModule } from './maretial.module';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialrModule
   ],
   providers: [],
   bootstrap: [AppComponent]
