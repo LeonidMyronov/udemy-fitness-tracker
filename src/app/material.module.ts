d0e597380d8bde3e98602faf6d0728958f882e82
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-03-25 21:19:19 +0300

    add training components. Add mat-tabs to main training component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,14 +8,14 @@ import {
   MatButtonModule,
   MatSidenavModule,
   MatIconModule,
-  MatToolbarModule, MatListModule
+  MatToolbarModule, MatListModule, MatTabsModule
 } from '@angular/material';
 
 @NgModule({
   imports: [MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatButtonModule,
-    MatSidenavModule, MatIconModule, MatToolbarModule, MatFormFieldModule, MatListModule],
+    MatSidenavModule, MatIconModule, MatToolbarModule, MatFormFieldModule, MatListModule, MatTabsModule],
   exports: [MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatButtonModule,
-    MatSidenavModule, MatIconModule, MatToolbarModule, MatFormFieldModule, MatListModule],
+    MatSidenavModule, MatIconModule, MatToolbarModule, MatFormFieldModule, MatListModule, MatTabsModule],
 })
 
 export class MaterialModule {}
---
+++ src/app/training/training.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ft-training',
+  templateUrl: './training.component.html',
+  styleUrls: ['./training.component.sass']
+})
+export class TrainingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
